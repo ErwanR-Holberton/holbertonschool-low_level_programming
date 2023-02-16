675e52cec390d89be1f64c0d0d7181ca925845ae
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-16 06:36:51 -0800

    task 04

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Print "Programming is like building a multilingual puzzle
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
