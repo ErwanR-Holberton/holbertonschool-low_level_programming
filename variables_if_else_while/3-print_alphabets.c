1e9820894697764f1f203d2805c3e390e5eee91d
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-20 04:09:56 -0800

    task 03

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,5 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
@@ -14,6 +10,9 @@ int main(void)
 	char n;
 
 	for (n = 'a' ; n <= 'z' ; n++)
+		putchar(n);
+
+	for (n = 'A' ; n <= 'Z' ; n++)
 		putchar(n);
 
 	printf("\n");
