6e928fdc0e9d414c6174733b81adbc96323d276c
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-20 07:40:27 -0800

    task 01

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,17 @@
 #include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Print alphabet and newline caracter
+ */
 void print_alphabet(void)
 {
-        char c;
-        for (c = 'a' ; c <= 'z' ; c++)
-        {
-                _putchar(c);
-        }
-        _putchar('\n');
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
