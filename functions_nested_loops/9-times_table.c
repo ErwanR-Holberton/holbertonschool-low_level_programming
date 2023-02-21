c67119be650b634e5816ffde06b79c19aed4be6a
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-21 05:24:23 -0800

    task 09

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ * print the time table
+ *
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9 ; b++)
+		{
+			if (a * b > 10)
+				_putchar(a * b / 10 + 48);
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar(a * b % 10 + 48);
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
