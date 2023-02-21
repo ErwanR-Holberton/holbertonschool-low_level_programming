c2b351547a13cfb713de46456e14711cdd7fbe4f
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-21 02:57:58 -0800

    task 06

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n : n is tested (+ - or 0 ?)
+ * Return: if lowercase
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * (-1));
+	}
+}
