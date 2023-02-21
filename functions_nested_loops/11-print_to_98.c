ce71553be089dcd5001b63aea8df350c758d4a65
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-21 07:29:57 -0800

    task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ * print ints n - 98
+ * @n : n is starting point
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			if (n <= -10)
+				_putchar((n / 10) * -1 + '0');
+			_putchar((n % 10) * -1 + '0');
+		}
+		else if (n >= 100)
+		{
+			_putchar(n / 100 + '0');
+			_putchar((n - 100) / 10 + '0');
+			_putchar(n % 10 + 48);
+		}
+		else if (n < 100)
+		{
+			if (n >= 10)
+				_putchar(n / 10 + '0');
+			_putchar(n % 10 + 48);
+		}
+		else
+			_putchar(n % 10 + '0');
+		_putchar(',');
+		_putchar(' ');
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
