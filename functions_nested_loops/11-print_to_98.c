3cb471d10abe720966f72b9a1efa2900366ab561
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-21 07:33:19 -0800

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 		if (n < 0)
 		{
 			_putchar('-');
-			if (n <= 100)
+			if (n <= -100)
 				_putchar((n / 100) * -1 + '0');
 			if (n <= -10)
 				_putchar((n / 10) * -1 + '0');
