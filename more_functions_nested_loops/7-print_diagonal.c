12698c429b0a636e23e7b7c1255d72decce9d303
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-22 07:26:02 -0800

    task 07

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,9 +10,11 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
-		for (j = i; j >= 0; j--)
+		for (j = i; j > 0; j--)
 			_putchar(' ');
 		_putchar('\\');
 		_putchar('\n');
