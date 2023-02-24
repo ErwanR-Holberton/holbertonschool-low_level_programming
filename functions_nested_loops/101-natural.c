eacec239760d7c31c3fc5c22709242dcf1db7ad4
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-24 07:50:59 -0800

    task 13

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if(i % 3 == 0)
+		if (i % 3 == 0)
 			sum = sum + i;
 		else if (i % 5 == 0)
 			sum = sum + i;
 	}
-	printf("%d\n",sum);
-    return (0);
+	printf("%d\n", sum);
+	return (0);
 }
