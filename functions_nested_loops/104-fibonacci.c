3943b1c4168cf764e268f1defa80469323d66e6a
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-24 11:04:16 -0800

    task 16

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i, numA = 0, numB = 1, tempo;
+
+	for (i = 0; i < 98; i++)
+	{
+		tempo = numA + numB;
+		numA = numB;
+		numB = tempo;
+		printf("%lu", numB);
+		if (i != 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
