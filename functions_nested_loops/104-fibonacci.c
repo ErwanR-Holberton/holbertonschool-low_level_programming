d49be07bab178345d54dcc45ddb615de9a41a096
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-24 12:53:30 -0800

    task 16

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,15 +7,25 @@
  */
 int main(void)
 {
-	long int i, numA = 0, numB = 1, tempo;
+	unsigned long int i, numA = 0, numB = 1;
+	unsigned long int highA = 0, highB = 0, tempo, hightempo = 0;
 
 	for (i = 0; i < 98; i++)
 	{
 		tempo = numA + numB;
+		hightempo = highA + highB;
+		if (tempo > 100000000000000)
+		{
+			hightempo = hightempo + tempo / 100000000000000;
+			tempo = tempo % 100000000000000;
+		}
 		numA = numB;
+		highA = highB;
 		numB = tempo;
+		highB = hightempo;
+		printf("%lu", highB);
 		printf("%lu", numB);
-		if (i != 49)
+		if (i != 97)
 			printf(", ");
 	}
 	printf("\n");
