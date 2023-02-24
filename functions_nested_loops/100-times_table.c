6779693a87cdcd725d186d6e058d564207280ec0
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-24 07:31:07 -0800

    task 12

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_times_table - Entry point
+ * print time table size n
+ * @n : n is table size
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 15)
+		n = 15;
+	if (n < 0)
+		n = 0;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (i * j >= 100)
+				_putchar(i * j / 100 + '0');
+			else if (j != 0)
+				_putchar(' ');
+			if (i * j >= 10)
+				_putchar(i * j / 10 % 10 + '0');
+			else if (j != 0)
+				_putchar(' ');
+			_putchar(i * j % 10 + '0');
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
