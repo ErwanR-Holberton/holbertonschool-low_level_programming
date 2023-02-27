06459becabe7db59c3821e385a7a8ff1908c09c6
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-27 06:06:52 -0800

    task 09

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (dest[i] != '\0')
+	while (dest[i])
 	{
-		printf("%c", dest[i]);
+/*		printf("%c", dest[i]);*/
 		src[i] = dest[i];
 		i++;
 	}
-	dest[i] = '\0';
+/*	dest[i] = '\0';*/
 	return (src);
 }
