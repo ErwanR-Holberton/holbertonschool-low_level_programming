2d435a7c3e2d004935caa3b51adede68578b6c0e
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-27 03:11:15 -0800

    task 08

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * @n : lenght
+ * @a : points to string start
+ * print half the string
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(" ,");
+	}
+	printf("\n");
+}
