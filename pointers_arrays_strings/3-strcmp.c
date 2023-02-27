e7b0301a30c295894303f9e82a2a6c46599ff598
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-27 12:58:09 -0800

    p2 task 03

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcmp - Entry point
+ *
+ * Concatenate
+ *
+ * @s1 : pointer to first string
+ * @s2 : pointer to second string
+ *
+ * Return: pointer to string start
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+	{
+		if ((s1[i] == '\0') || (s1[i] == '\0'))
+			return (0);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
