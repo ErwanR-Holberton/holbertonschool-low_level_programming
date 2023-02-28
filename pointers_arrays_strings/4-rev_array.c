3d39ce29a6e7e3e4a33ce79f36f2dd1241160551
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-28 00:21:00 -0800

    p2 task 04

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ *
+ * Concatenate
+ *
+ * @a : pointer to first string
+ * @n : n of items in array
+ *
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, tempo = 0;
+
+	for (i = 0; i <= n / 2 ; i++)
+	{
+		tempo = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tempo;
+	}
+}
