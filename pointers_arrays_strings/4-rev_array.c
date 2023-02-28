f813350d29e738c5aa4156f5a4fd1133691f2ef8
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-28 00:24:58 -0800

    p2 task 04

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -15,10 +15,12 @@ void reverse_array(int *a, int n)
 {
 	int i = 0, tempo = 0;
 
+	if (n % 2)
+		n = n - 1;
 	for (i = 0; i <= n / 2 ; i++)
 	{
 		tempo = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = tempo;
+		a[i] = a[n - i];
+		a[n - i] = tempo;
 	}
 }
