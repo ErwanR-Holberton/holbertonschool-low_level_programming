12470cbce1f1c0c010835ff175401ee614794564
ErwanR-Holberton <6218@holbertonstudents.com>
2023-02-28 01:12:16 -0800

    p2 task 06

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,25 +13,27 @@
  */
 char *cap_string(char *a)
 {
-	int i = 0, separator = 0;
+	int i = 0, separator = 1;
 
 	while (a[i] != '\0')
 	{
 		if ((a[i] == ',') || (a[i] == ';') || (a[i] == '.'))
 			separator = 1;
-		if ((a[i] == '!') || (a[i] == '?') || (a[i] == '\"'))
+		else if ((a[i] == '!') || (a[i] == '?') || (a[i] == '\"'))
 			separator = 1;
-		if ((a[i] == '(') || (a[i] == ')') || (a[i] == '{') || (a[i] == '}'))
+		else if ((a[i] == '(') || (a[i] == ')') || (a[i] == '{') || (a[i] == '}'))
 			separator = 1;
-		if ((a[i] >= 'A') && (a[i] <= 'Z') && separator)
+		else if ((a[i] == ' ') || (a[i] == '\t') || (a[i] == '\n'))
+			separator = 1;
+		else if ((a[i] >= 'A') && (a[i] <= 'Z') && separator)
 			separator = 0;
-		if ((a[i] == ' ') || (a[i] == '\t') || (a[i] == '\n'))
-			separator = 1;
-		if ((a[i] >= 'a') && (a[i] <= 'z') && separator)
+		else if ((a[i] >= 'a') && (a[i] <= 'z') && separator)
 		{
 			a[i] = a[i] - 'a' + 'A';
 			separator = 0;
 		}
+		else
+			separator = 0;
 		i++;
 	}
 	return (a);
