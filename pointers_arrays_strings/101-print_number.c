5eecaf83a0550248c7cd52f535a25217b75d5631
ErwanR-Holberton <6218@holbertonstudents.com>
2023-03-01 07:59:17 -0800

    p2 task 09

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ *
+ * print number with putchar
+ *
+ * @n : number to print
+ *
+ *
+ */
+void print_number(int n)
+{
+	int num = n, lenght = 1, exposant = 1;
+
+	for (lenght = 0; num / 10 != 0; lenght++)
+	{
+		num = num / 10;
+		exposant *= 10;
+	}
+	num = n;
+	if (num < 0)
+	{
+		num *= -1;
+		_putchar('-');
+	}
+	for (; lenght >= 0; lenght--)
+	{
+		_putchar(num / exposant + '0');
+		num = num - (num / exposant * exposant);
+		exposant = exposant / 10;
+	}
+
+}
