8cd965a32ae5bd8aa79e927bf28cc8077ba99058
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-14 06:02:12 -0700

    t6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(a));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
-	printf("Size of a long long: %ld byte(s)\n", sizeof(ll));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
