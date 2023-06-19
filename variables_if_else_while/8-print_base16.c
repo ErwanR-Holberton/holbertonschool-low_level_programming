e43f09c8f044e55f7880e83cc3b52d92b0c1ef83
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-19 02:28:59 -0700

    t8 fix A to a

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	char alpha;
 
-	for (alpha = '0'; alpha <= 'F'; alpha++)
+	for (alpha = '0'; alpha <= 'f'; alpha++)
 	{
 		putchar(alpha);
 		if (alpha == '9')
-			alpha = 'A' - 1;
+			alpha = 'a' - 1;
 	}
 	putchar('\n');
 	return (0);
