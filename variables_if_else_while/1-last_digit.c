985a2519d748127160e10ee7dea412280a80932e
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-19 01:30:43 -0700

    t1 fix missing space

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	LastDigit = n % 10;
 
-	printf("Last digit of %d is %d and is", n, LastDigit);
+	printf("Last digit of %d is %d and is ", n, LastDigit);
 	if (LastDigit > 5)
 		printf("greater than 5\n");
 	else if (LastDigit == 0)
