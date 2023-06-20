36bf79d0cf460e2d26fadd804179859dd7575c2a
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-20 05:24:24 -0700

    t2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 0; alpha < 10; alpha++)
+		print_alphabet();
+}
