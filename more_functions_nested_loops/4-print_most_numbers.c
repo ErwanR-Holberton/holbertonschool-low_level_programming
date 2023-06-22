a1e0512fc8751b2d554cfc4c37f23e56d2dfa719
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-22 09:44:27 -0700

    t4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 {
 	int count;
 
-        for (count = 0; count < 10; count++)
+	for (count = 0; count < 10; count++)
 		if (count != 2 && count != 4)
 			_putchar(count + '0');
 	_putchar('\n');
