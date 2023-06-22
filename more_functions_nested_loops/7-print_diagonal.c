c74c94b5047bb27d871581c18faf4c3c5ee547af
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-22 11:13:57 -0700

    t7 fix newline

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,18 @@ void print_diagonal(int n)
 {
 	int lines = 0, col;
 
-	do {
-		for (col = 0; col <= lines; col++)
+	if (n > 0)
+		for (lines = 0; lines < n; lines++)
 		{
-			if (lines == col && n > 0)
-				_putchar('\\');
-			else
-				_putchar(' ');
+			for (col = 0; col <= lines; col++)
+			{
+				if (lines == col && n > 0)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
+	else
 		_putchar('\n');
-		lines++;
-	} while(lines < n);
 }
