9d8e436880b596fd508bbd0bbe462904b48c8a9e
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-22 09:31:00 -0700

    t3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - check the code.
+ * print_numbers - check the code.
  * @c: the character to test
  * Return: Always 0.
  */
-int _isupper(int c)
+void print_numbers(void)
 {
 	return (c >= 'A' && c <= 'Z');
 }
