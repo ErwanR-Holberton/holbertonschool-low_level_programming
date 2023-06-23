7721d869d771d9ae81b59586bf0457b40bca8cb2
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-23 06:58:20 -0700

    t3 fix betty

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - check the code.
- * @c: the character to test
+ *
  * Return: Nothing
  */
 void print_numbers(void)
 {
 	int count;
 
-        for (count = 0; count < 10; count++)
-                _putchar(count + '0');
+	for (count = 0; count < 10; count++)
+		_putchar(count + '0');
 	_putchar('\n');
 }
