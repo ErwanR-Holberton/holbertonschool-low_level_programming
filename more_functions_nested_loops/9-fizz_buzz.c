a6ca25214a3f0e6475a7f17c77903c76d8010f61
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-23 04:30:16 -0700

    t9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int count;
 
-	for ( count = 1; count <= 100; count++)
+	for (count = 1; count <= 100; count++)
 	{
-		if (count %3 == 0)
+		if (count % 3 == 0)
 			printf("Fizz");
-		if (count %5 == 0)
+		if (count % 5 == 0)
 			printf("Buzz");
-		if (count %3 != 0 && count %5 != 0)
+		if (count % 3 != 0 && count % 5 != 0)
 			printf("%d", count);
 		if (count != 100)
 			printf(" ");
