5ddc556fca37457e63f3847419ce9992e4558cb7
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-26 01:32:33 -0700

    t4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: start of string
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+
+	while (count >= 0)
+	{
+		_putchar(s[count]);
+		count--;
+	}
+	_putchar('\n');
+}
