f7ff05b26afab0203146bb0dc85dae4875481661
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-26 20:12:42 +0200

    t10 atoi de jouer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,25 +4,25 @@
  * _atoi - check the code
  * @s: string start
  *
- * Return: adress of destination
+ * Return: first nuber in string with sign
  */
 int _atoi(char *s)
 {
-	int count, sign, value = 0, valuefound = 0;
+	int count, sign = 1, value = 0, valuefound = 0;
 
 	for (count = 0; s[count] != '\0'; count++)
 	{
 		if (s[count] >= '0' && s[count] <= '9' && valuefound == 0)
 		{
-			value = value * 10 + s[count];
+			value = value * 10 + s[count] - '0';
 			if (s[count + 1] < '0' || s[count + 1] > '9')
 				valuefound = 1;
 		}
 		else if (s[count] == '-' && valuefound == 0)
 			sign = -1;
 		else if (s[count] == '+' && valuefound == 0)
-			sign = 1
+			sign = 1;
 
-
-		return (dest);
 	}
+	return (sign * value);
+}
