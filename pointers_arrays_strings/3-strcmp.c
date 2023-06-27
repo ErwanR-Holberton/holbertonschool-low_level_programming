36c9d9d33592187dacad4bd887362d34d09c35bc
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-27 17:19:25 +0200

    t3 fix line too long

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int count;
+	int i;
 
-	for (count = 0; s1[count] == s2[count] && s1[count] != '\0' && s2[count] != '\0'; )
-		count++;
+	for (i = 0; s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0'; )
+		i++;
 
 
-	return (s1[count] - s2[count]);
+	return (s1[i] - s2[i]);
 }
