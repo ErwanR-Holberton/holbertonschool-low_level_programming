92d003292deefa5184a72a4bf60234a32946f752
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-28 00:03:57 +0200

    t8 fixed to respect only 1 if

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -8,14 +8,28 @@
  */
 char *rot13(char *str)
 {
-	int i;
+	int i, j;
+	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = (str[i] - 'a' + 13) % 26 + 'a';
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] = (str[i] - 'A' + 13) % 26 + 'A';
+		for (j = 0; j < 2 * 26; j++)
+		{
+			if (letters[j] == str[i])
+			{
+				j = ((j - (26 * j > 25) +13) % 26 )+ (26 * j > 25);
+				str[i] = letters[j];
+			}
+		}
 	}
 	return (str);
 }
+/*
+for (j = 0; str[i] != majs[j] && str[i] != mins[j]; )
+	j++;
+
+if (str[i] == majs[j])
+	str[i] = (str[i] - 'A' + 13) % 26 + 'A';
+else
+	str[i] = (str[i] - 'a' + 13) % 26 + 'a';
+*/
