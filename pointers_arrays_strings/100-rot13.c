76b6f9e0937794252d09ebdb45084a80a773ee93
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-28 10:12:19 +0200

    t8 fix betty

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -17,19 +17,10 @@ char *rot13(char *str)
 		{
 			if (letters[j] == str[i])
 			{
-				j = ((j - (26 * (j > 25)) +13) % 26 )+ (26 * (j > 25));
+				j = ((j - (26 * (j > 25)) + 13) % 26) + (26 * (j > 25));
 				str[i] = letters[j];
 			}
 		}
 	}
 	return (str);
 }
-/*
-for (j = 0; str[i] != majs[j] && str[i] != mins[j]; )
-	j++;
-
-if (str[i] == majs[j])
-	str[i] = (str[i] - 'A' + 13) % 26 + 'A';
-else
-	str[i] = (str[i] - 'a' + 13) % 26 + 'a';
-*/
