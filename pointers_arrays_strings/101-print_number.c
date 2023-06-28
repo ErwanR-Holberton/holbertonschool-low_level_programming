181814b625c55c9216feef6385b38504d38e0cb0
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-28 12:26:36 +0200

    t9

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - check the code
+ * @n: number to print
+ *
+ * Return: NOTHING
+ */
+void print_number(int n)
+{
+	int power10 = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	for (;n > power10 * 10; )
+		power10 = power10 * 10;
+
+	while (power10 > 0)
+	{
+		_putchar(n / power10 % 10 + '0');
+		power10 = power10 / 10;
+	}
+
+}
