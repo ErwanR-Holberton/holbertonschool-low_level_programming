c51f833dd7ed60c2b4a8d17d7cfb64a1ea5fac81
ErwanR-Holberton <6218@holbertonstudents.com>
2023-06-29 12:28:42 +0200

    t0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - check the code
+ * @s: start of string
+ * @b: byte to fill the string with
+ * @n: number of bytes to fill
+ * Return: start of string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	s[i] = '\0';
+	return (s);
+}
