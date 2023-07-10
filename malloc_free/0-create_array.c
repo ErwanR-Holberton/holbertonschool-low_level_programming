a59d80988ccc17fa762238c61fb8867b09530350
ErwanR-Holberton <6218@holbertonstudents.com>
2023-07-10 09:13:40 +0200

    T0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array - check the code.
+ *
+ * Create an array with a size and initialisez to c
+ *
+ *
+ * @size: the character to test
+ * @c: char to initialize the array with
+ * Return: Pointer to array or null if faillure
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(size);
+	if (array == NULL)
+		return (0);
+
+	size--;
+	for (;size != 0; size--)
+		array[size] = c;
+	array[0] = c;
+
+	return (array);
+}
