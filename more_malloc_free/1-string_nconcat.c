43f19dad455bb29463b6217ef2e7906e61d32ab3
ErwanR-Holberton <6218@holbertonstudents.com>
2023-07-11 15:23:00 +0200

    T1 fix all

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,17 +18,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 
 	if (s1 != NULL)
-		for (; s1[size1] != '\0';)
+		for (size1 = 0; s1[size1] != '\0';)
 			size1++;
-
+	size2 = 0;
 	if (s2 != NULL)
-		for (; s2[size2] != '\0';)
+		for (size2 = 0; s2[size2] != '\0';)
 			size2++;
+
 
 	if (n > size2)
 		n = size2;
 
-	newstr = malloc(size1 + n + 1);
+	newstr = malloc(sizeof(char) * (size1 + n + 1));
 
 	if (newstr == NULL)
 		return (NULL);
@@ -40,6 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < n; i++)
 		newstr[size1 + i] = s2[i];
 
-
+	newstr[size1 + n] = 0;
+	
 	return (newstr);
 }
