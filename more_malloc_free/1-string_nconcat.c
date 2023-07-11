a68da3444a57a5a3cbc2b0307c1048c9003a91bd
ErwanR-Holberton <6218@holbertonstudents.com>
2023-07-11 13:36:05 +0200

    T1 fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (size2 > n)
 	{
 		size2 = n - 1;
-		newstr = malloc(size1 + n + 1);
+		newstr = malloc(size1 + size2 + 1);
 	}
 	else
 		newstr = malloc(size1 + size2 + 1);
