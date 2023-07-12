c93174a2e90a3254a852a106c3ed5afb7a3616c7
ErwanR-Holberton <6218@holbertonstudents.com>
2023-07-12 15:08:21 +0200

    T2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * *_calloc - check the code.
+ *
+ * allocates memory for an array of nmemb elements of size bytes each
+ *
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: pointer to the allocated memory or null if faillure
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__)
+	return (0);
+}
+
