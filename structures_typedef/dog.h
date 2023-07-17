d9eb59692237e22d89af622881685f21d0c1d1f9
ErwanR-Holberton <6218@holbertonstudents.com>
2023-07-17 09:47:57 +0200

    T0 fix description

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG
 #define DOG
+/**
+ * dog - Short description
+ * @name: name of the dog(s)
+ * @age: age of the dog(s)
+ * @owner: owner of the dog(s)
+ *
+ * Structure containing all of the dog(s) information
+ */
 struct dog
 {
 	char *name;
