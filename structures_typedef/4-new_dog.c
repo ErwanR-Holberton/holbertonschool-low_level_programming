7eeddb07b379e12fead29c07e92579469caa64aa
ErwanR-Holberton <6218@holbertonstudents.com>
2023-07-17 14:06:14 +0200

    T4 fix copy string

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,11 +10,19 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	long unsigned int i;
+
 	struct dog *my_dog2 = malloc(sizeof(struct dog));
 	if (my_dog2 == 0)
 		return (NULL);
-	(*my_dog2).name = name;
+	(*my_dog2).name = malloc(sizeof(name));
 	(*my_dog2).age = age;
-	(*my_dog2).owner = owner;
+	(*my_dog2).owner = malloc(sizeof(owner));
+	if ((*my_dog2).name == 0 || (*my_dog2).owner == 0)
+		return (NULL);
+	for (i = 0; i <= sizeof(name); i++)
+		(*my_dog2).name[i] = name[i];
+	for (i = 0; i <= sizeof(owner); i++)
+		(*my_dog2).owner[i] = owner[i];
 	return(my_dog2);
 }
