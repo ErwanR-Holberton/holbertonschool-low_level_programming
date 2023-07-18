ef72d5ef67b8e515538ce9b522aff17cdc2f9cfd
ErwanR-Holberton <6218@holbertonstudents.com>
2023-07-18 23:13:18 +0200

    T3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - add a and b
+ * @a: first number
+ * @b: second number
+ * Return: the sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract b from a
+ * @a: first number
+ * @b: second number
+ * Return: a - b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies a and b
+ * @a: first number
+ * @b: second number
+ * Return: a * b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide a by b
+ * @a: first number
+ * @b: second number
+ * Return: a / b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - does a modulo b
+ * @a: first number
+ * @b: second number
+ * Return: the rest of a / b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
