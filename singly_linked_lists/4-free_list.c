58ad9130e4f01e329e3605ab6ea18b19fcdf35f0
ErwanR-Holberton <6218@holbertonstudents.com>
2023-07-31 15:30:42 +0200

    T4 initial push

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - free the list
+ * @head: points to the start of a list
+ *
+ * Return: NOTHING
+ */
+void free_list(list_t *head)
+{
+	list_t *temp_next, *temp_current = head;
+	while (temp_current != NULL)
+	{
+		temp_next = temp_current->next;
+		free(temp_current->str);
+		free(temp_current);
+		temp_current = temp_next;
+	}
+}
