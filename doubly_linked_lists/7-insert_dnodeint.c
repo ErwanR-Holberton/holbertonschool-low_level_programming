ffefcb7ade06c6a6fcb5c0a72414b490dc6a1bd4
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-01 13:03:35 +0200

    T7 inverted order of if and i++;

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,10 +17,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	while (copyhead != NULL)
 	{
-
-
-		copyhead = copyhead->next;
-		i++;
 		if (i == idx)
 		{
 			newnode->n = n;
@@ -30,6 +26,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			newnode->prev->next = newnode;
 			return (newnode);
 		}
+		copyhead = copyhead->next;
+		i++;
 	}
 	return (NULL);
 }
