0ce0ccf85999564100418a705d1e62a130cb8da8
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-02 18:46:17 +0200

    T1 done

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_binary - print the binary representation of n
+ * @n: the value to print in binary
+ *
+ * Description: print the binary representation of n
+ * Return: NOTHING
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+
+	for (i = 63; n >> i == 0 && i > 0; )
+		i--;
+	for (; i >= 0; i--)
+	{
+		_putchar((n >> i) + '0');
+		n = n - ((n >> i) << i);
+	}
+	_putchar('\n');
+}
