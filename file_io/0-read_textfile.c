3dad09e603fccd1bfbb81733e445578812ee930d
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-07 11:21:32 +0200

    T0 fixed condition to -1

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	descriptor = open(filename, O_RDONLY);
 
-	if (descriptor)
+	if (descriptor == -1)
 		return (0);
 
 	while (count < letters)
