9cfd6b4628bbb2eb4b83af926cf0e1da48b332a4
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-07 11:18:45 +0200

    T0 fixed check if open and simplified

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,14 +9,15 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int is_opened, descriptor, nb_read, n = 10;
+	int descriptor, nb_read, n = 10;
 	size_t count = 0;
 	char buffer[10];
 
-	is_opened = open(filename, O_RDONLY);
-	descriptor = is_opened;
+	if (filename == NULL)
+		return (0);
+	descriptor = open(filename, O_RDONLY);
 
-	if (!is_opened)
+	if (descriptor)
 		return (0);
 
 	while (count < letters)
