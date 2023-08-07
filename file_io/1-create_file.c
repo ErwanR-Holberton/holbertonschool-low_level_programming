728732ac89124835c768d4d9a860693cbcb24600
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-07 14:58:10 +0200

    T1 fixed trunc

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,11 +13,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_descriptor = open(filename, O_CREAT | O_RDWR, 0600);
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
 	if (text_content)
-		chars_printed = write(file_descriptor, text_content, strlen(text_content));
+		chars_printed = write(file_descriptor, text_content, strlen(text_content) + 1);
 	close(file_descriptor);
 	if (chars_printed == -1)
 		return (-1);
