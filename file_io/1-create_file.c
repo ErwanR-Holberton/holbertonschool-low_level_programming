54af628da2be5eaac0a6f2c8c4f67f3b41fac8d1
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-07 14:53:37 +0200

    T1 fixed return values

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,12 +11,16 @@ int create_file(const char *filename, char *text_content)
 {
 	int file_descriptor, chars_printed;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	file_descriptor = open(filename, O_CREAT | O_RDWR, 0600);
 	if (file_descriptor == -1)
 		return (-1);
-	chars_printed = write(file_descriptor, text_content, strlen(text_content));
+	if (text_content)
+		chars_printed = write(file_descriptor, text_content, strlen(text_content));
 	close(file_descriptor);
-	return (chars_printed);
+	if (chars_printed == -1)
+		return (-1);
+	else
+		return (1);
 }
