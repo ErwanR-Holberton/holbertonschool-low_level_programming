4f5ca31acb24bf7ef4e2450e5a61e3c0ce689fbb
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-18 19:54:40 +0200

    Added function binary search

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+#include <unistd.h>
+/**
+ * binary_search - search in an array using binary algorithm
+ * @array: array to search
+ * @size: size of the array
+ * @value: value to find
+ * Return: index found or -1 (fail)
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int range = size, i, left = 0, right = size - 1;
+
+	while (range != 0)
+	{
+		printf("Searching in array:");
+		for (i = left; i <= right; i++)
+			printf(" %d", i);
+		printf("\n");
+		i = (left + right) / 2;
+		if (array[i] == value)
+			return (i);
+		else if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+		range = right - left;
+	}
+	return (-1);
+}
